24c56b7c73e8f0e882430cf52bcf32b0836729a9
FelixMcFelix <FelixMcFelix@users.noreply.github.com>
2020-11-16 09:24:08 +0000

    deploy: 9b43f6cf1d554c5cb81d57edcc405c276af9a6d7

--- next/implementors/core/ops/deref/trait.Deref.js
+++ next/implementors/core/ops/deref/trait.Deref.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["songbird"] = [{"text":"impl Deref for Call","synthetic":false,"types":[]}];
+implementors["songbird"] = [{"text":"impl Deref for Call","synthetic":false,"types":[]},{"text":"impl Deref for Queued","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
