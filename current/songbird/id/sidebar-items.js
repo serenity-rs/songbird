1f52d6396c11864f517bee22a4bac30e7290eb6f
FelixMcFelix <FelixMcFelix@users.noreply.github.com>
2021-03-11 23:01:37 +0000

    deploy: b9a926c1254b44d450f00eb161139fdd6f6bbbd1

--- current/songbird/id/sidebar-items.js
+++ current/songbird/id/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["ChannelId","ID of a Discord voice/text channel."],["GuildId","ID of a Discord guild (colloquially, \"server\")."],["UserId","ID of a Discord user."]]});
+initSidebarItems({"struct":[["ChannelId","ID of a Discord voice/text channel."],["GuildId","ID of a Discord guild (colloquially, “server”)."],["UserId","ID of a Discord user."]]});
