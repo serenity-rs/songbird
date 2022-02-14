44a7c0d2c57e9be9928a91ae8a0d0dca87f4ef90
Kyle Simpson <kyleandrew.simpson@gmail.com>
2022-02-14 12:23:42 +0000

    Events: Remove deprecated events.
    This removes the `ClientConnect`, `DriverConnectFailed`, `DriverReconnectFailed` and `SsrcKnown` events.
    
    Tested using `cargo make ready`.

--- src/driver/tasks/mod.rs
+++ src/driver/tasks/mod.rs
@@ -331,10 +331,6 @@ impl ConnectionRetryData {
                             let _ = tx.send(Err(why));
 
                             let _ = interconnect.events.send(EventMessage::FireCoreEvent(
-                                CoreContext::DriverConnectFailed,
-                            ));
-
-                            let _ = interconnect.events.send(EventMessage::FireCoreEvent(
                                 CoreContext::DriverDisconnect(InternalDisconnect {
                                     kind: DisconnectKind::Connect,
                                     reason,
@@ -343,10 +339,6 @@ impl ConnectionRetryData {
                             ));
                         },
                         ConnectionFlavour::Reconnect => {
-                            let _ = interconnect.events.send(EventMessage::FireCoreEvent(
-                                CoreContext::DriverReconnectFailed,
-                            ));
-
                             let _ = interconnect.events.send(EventMessage::FireCoreEvent(
                                 CoreContext::DriverDisconnect(InternalDisconnect {
                                     kind: DisconnectKind::Reconnect,
