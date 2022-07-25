0beb0f0d760fbd84f3f4abcb68fa8e71a667e896
Gnome! <45660393+GnomedDev@users.noreply.github.com>
2022-07-25 14:20:58 +0100

    Input: lazy_static -> once_cell::sync::Lazy (#136)

--- src/input/codecs/mod.rs
+++ src/input/codecs/mod.rs
@@ -5,30 +5,25 @@ mod opus;
 mod raw;
 
 pub use self::{dca::DcaReader, opus::OpusDecoder, raw::*};
-use lazy_static::lazy_static;
+use once_cell::sync::Lazy;
 use symphonia::{
     core::{codecs::CodecRegistry, probe::Probe},
     default::*,
 };
 
-lazy_static! {
-    /// Default Symphonia CodecRegistry, including the (audiopus-backed)
-    /// Opus codec.
-    pub static ref CODEC_REGISTRY: CodecRegistry = {
-        let mut registry = CodecRegistry::new();
-        register_enabled_codecs(&mut registry);
-        registry.register_all::<OpusDecoder>();
-        registry
-    };
-}
+/// Default Symphonia [`CodecRegistry`], including the (audiopus-backed) Opus codec.
+pub static CODEC_REGISTRY: Lazy<CodecRegistry> = Lazy::new(|| {
+    let mut registry = CodecRegistry::new();
+    register_enabled_codecs(&mut registry);
+    registry.register_all::<OpusDecoder>();
+    registry
+});
 
-lazy_static! {
-    /// Default Symphonia Probe, including DCA format support.
-    pub static ref PROBE: Probe = {
-        let mut probe = Probe::default();
-        probe.register_all::<DcaReader>();
-        probe.register_all::<RawReader>();
-        register_enabled_formats(&mut probe);
-        probe
-    };
-}
+/// Default Symphonia Probe, including DCA format support.
+pub static PROBE: Lazy<Probe> = Lazy::new(|| {
+    let mut probe = Probe::default();
+    probe.register_all::<DcaReader>();
+    probe.register_all::<RawReader>();
+    register_enabled_formats(&mut probe);
+    probe
+});
