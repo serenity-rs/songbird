63c61d47ba95208f4aaeca65cf60e5610d8e39ae
FelixMcFelix <FelixMcFelix@users.noreply.github.com>
2022-09-07 09:29:09 +0000

    deploy: eedab8f69d1c17125971e290ee8a50fc1adcdffc

--- current/implementors/core/cmp/trait.PartialOrd.js
+++ current/implementors/core/cmp/trait.PartialOrd.js
@@ -1,3 +1,3 @@
-(function() {var implementors = {};
-implementors["songbird"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a>&lt;<a class=\"struct\" href=\"songbird/events/struct.EventData.html\" title=\"struct songbird::events::EventData\">EventData</a>&gt; for <a class=\"struct\" href=\"songbird/events/struct.EventData.html\" title=\"struct songbird::events::EventData\">EventData</a>","synthetic":false,"types":["songbird::events::data::EventData"]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
+(function() {var implementors = {
+"songbird":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a>&lt;<a class=\"struct\" href=\"songbird/events/struct.EventData.html\" title=\"struct songbird::events::EventData\">EventData</a>&gt; for <a class=\"struct\" href=\"songbird/events/struct.EventData.html\" title=\"struct songbird::events::EventData\">EventData</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- current/implementors/typemap_rev/trait.TypeMapKey.js
+++ current/implementors/typemap_rev/trait.TypeMapKey.js
@@ -1,3 +1,3 @@
-(function() {var implementors = {};
-implementors["songbird"] = [{"text":"impl TypeMapKey for <a class=\"struct\" href=\"songbird/serenity/struct.SongbirdKey.html\" title=\"struct songbird::serenity::SongbirdKey\">SongbirdKey</a>","synthetic":false,"types":["songbird::serenity::SongbirdKey"]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
+(function() {var implementors = {
+"songbird":[["impl TypeMapKey for <a class=\"struct\" href=\"songbird/serenity/struct.SongbirdKey.html\" title=\"struct songbird::serenity::SongbirdKey\">SongbirdKey</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
