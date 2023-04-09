42afec5d546267188b96914c65eb2a30cf547eba
Zero <zero@imaskeleton.me>
2023-04-09 13:19:17 -0600

    revert last fixes and add new fix

--- src/shards.rs
+++ src/shards.rs
@@ -15,7 +15,7 @@ use serenity::client::bridge::gateway::ShardRunnerMessage;
 #[cfg(feature = "serenity")]
 use std::result::Result as StdResult;
 use std::sync::Arc;
-#[cfg(all(feature = "serenity", feature = "driver"))]
+#[cfg(feature = "serenity")]
 use tokio_tungstenite::tungstenite::Message;
 use tracing::{debug, error};
 #[cfg(feature = "twilight")]
