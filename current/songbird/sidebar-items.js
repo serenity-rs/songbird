b0d3b1a32feadc376331e71bbae6bea44bda5aac
FelixMcFelix <FelixMcFelix@users.noreply.github.com>
2023-04-09 08:36:48 +0000

    deploy: e5d3febb7bfbc6b4b98af3dbf312c23528307544

--- current/songbird/sidebar-items.js
+++ current/songbird/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["CoreEvent","EventContext","TrackEvent"],"fn":["ffmpeg","ytdl"],"mod":["constants","driver","error","events","id","input","join","serenity","shards","tracks"],"struct":["Call","Config","ConnectionInfo","Songbird"]};
+window.SIDEBAR_ITEMS = {"enum":["CoreEvent","TrackEvent"],"fn":["ffmpeg","ytdl"],"mod":["constants","driver","error","events","id","input","join","serenity","shards","tracks"],"struct":["Call","Config","ConnectionInfo","Songbird"]};
