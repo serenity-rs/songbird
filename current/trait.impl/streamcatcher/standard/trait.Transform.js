1fdc551cae82b17b2798cd8c78a580c4a45ff094
FelixMcFelix <FelixMcFelix@users.noreply.github.com>
2023-12-12 08:29:35 +0000

    deploy: d681b71b1f29dba7a8ac4fc6257529325763992b

--- current/trait.impl/streamcatcher/standard/trait.Transform.js
+++ current/trait.impl/streamcatcher/standard/trait.Transform.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"songbird":[["impl&lt;T&gt; Transform&lt;T&gt; for <a class=\"struct\" href=\"songbird/input/cached/struct.OpusCompressor.html\" title=\"struct songbird::input::cached::OpusCompressor\">OpusCompressor</a><span class=\"where fmt-newline\">where\n    T: <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/io/trait.Read.html\" title=\"trait std::io::Read\">Read</a>,</span>"]]
+"songbird":[["impl&lt;T&gt; Transform&lt;T&gt; for <a class=\"struct\" href=\"songbird/input/cached/struct.OpusCompressor.html\" title=\"struct songbird::input::cached::OpusCompressor\">OpusCompressor</a><div class=\"where\">where\n    T: <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/io/trait.Read.html\" title=\"trait std::io::Read\">Read</a>,</div>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
