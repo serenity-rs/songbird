707329d0107c117afcdab71b9a9af5fd5c3b03f2
FelixMcFelix <FelixMcFelix@users.noreply.github.com>
2025-02-21 22:14:40 +0000

    deploy: 84accb0e3a0c07f5a05065d96871123a3ebc7d30

--- next/trait.impl/core/iter/traits/iterator/trait.Iterator.js
+++ next/trait.impl/core/iter/traits/iterator/trait.Iterator.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["songbird",[["impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/iterator/trait.Iterator.html\" title=\"trait core::iter::traits::iterator::Iterator\">Iterator</a> for <a class=\"struct\" href=\"songbird/struct.Iter.html\" title=\"struct songbird::Iter\">Iter</a>&lt;'a&gt;"]]]]);
+    var implementors = Object.fromEntries([["songbird",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/iterator/trait.Iterator.html\" title=\"trait core::iter::traits::iterator::Iterator\">Iterator</a> for <a class=\"struct\" href=\"songbird/struct.Iter.html\" title=\"struct songbird::Iter\">Iter</a>&lt;'_&gt;"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[319]}
+//{"start":57,"fragment_lengths":[309]}
