ace67c599452841c944fd27c04696f2df08a9b4a
FelixMcFelix <FelixMcFelix@users.noreply.github.com>
2025-05-20 18:00:49 +0000

    deploy: c910d7087dc86094677ec3e67a068d9eefe239df

---
+++ current/trait.impl/serde/ser/trait.Serialize.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["songbird",[["impl <a class=\"trait\" href=\"https://docs.rs/serde/1.0.219/serde/ser/trait.Serialize.html\" title=\"trait serde::ser::Serialize\">Serialize</a> for <a class=\"struct\" href=\"songbird/input/metadata/struct.YoutubeDlOutput.html\" title=\"struct songbird::input::metadata::YoutubeDlOutput\">Output</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[320]}
