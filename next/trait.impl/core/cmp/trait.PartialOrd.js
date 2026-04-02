a5439b54d25e3ccf829138ffb17de99ce4ac9228
arqunis <arqunis@users.noreply.github.com>
2026-04-02 20:04:35 +0000

    deploy: d2561d789268c3706b558b9760851fc4f9ba2137

--- next/trait.impl/core/cmp/trait.PartialOrd.js
+++ next/trait.impl/core/cmp/trait.PartialOrd.js
@@ -1,9 +1,9 @@
 (function() {
-    const implementors = Object.fromEntries([["songbird",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a> for <a class=\"struct\" href=\"songbird/events/struct.EventData.html\" title=\"struct songbird::events::EventData\">EventData</a>",0]]]]);
+    const implementors = Object.fromEntries([["songbird",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a> for <a class=\"struct\" href=\"songbird/events/struct.EventData.html\" title=\"struct songbird::events::EventData\">EventData</a>",0],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a> for <a class=\"struct\" href=\"songbird/struct.FloatDuration.html\" title=\"struct songbird::FloatDuration\">FloatDuration</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":59,"fragment_lengths":[301]}
+//{"start":59,"fragment_lengths":[585]}
