4ea84ae1bde29aba9f98fda0d08c97ad1069d4a0
arqunis <arqunis@users.noreply.github.com>
2026-04-03 14:13:29 +0000

    deploy: 09036b15406cc3730842df7fdbf9fb8b8e966a38

---
+++ next/songbird/input/metadata/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"struct":["AuxMetadata","Metadata","YoutubeDlOutput"]};
---
+++ next/trait.impl/serde_core/ser/trait.Serialize.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["songbird",[["impl <a class=\"trait\" href=\"https://docs.rs/serde_core/1.0.228/serde_core/ser/trait.Serialize.html\" title=\"trait serde_core::ser::Serialize\">Serialize</a> for <a class=\"struct\" href=\"songbird/input/metadata/struct.YoutubeDlOutput.html\" title=\"struct songbird::input::metadata::YoutubeDlOutput\">Output</a>",0]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[337]}
