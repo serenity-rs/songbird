2f6c638c9aec363303cb91ef44e96b9bbfd4d6bf
arqunis <arqunis@users.noreply.github.com>
2026-04-05 12:07:54 +0000

    deploy: 3f77b5fa6de3f7354fdd7ef0dfa861518d304cdd

---
+++ current/trait.impl/enum_map/internal/trait.EnumArray.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["songbird",[["impl&lt;V&gt; EnumArray&lt;V&gt; for <a class=\"enum\" href=\"songbird/events/enum.CoreEvent.html\" title=\"enum songbird::events::CoreEvent\">CoreEvent</a>",0],["impl&lt;V&gt; EnumArray&lt;V&gt; for <a class=\"enum\" href=\"songbird/events/enum.TrackEvent.html\" title=\"enum songbird::events::TrackEvent\">TrackEvent</a>",0],["impl&lt;V&gt; EnumArray&lt;V&gt; for <a class=\"enum\" href=\"songbird/events/enum.UntimedEvent.html\" title=\"enum songbird::events::UntimedEvent\">UntimedEvent</a>",0]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[515]}
